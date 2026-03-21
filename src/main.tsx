55c13fda477e759dce913e0106142387ebbcbad5
Natalia_Tolstykh <kopylova9624@gmail.com>
2026-03-21 19:18:49 +0100

    fixed

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "modern-normalize";
-import App from "../src/Components/App/App";
+import App from "../src/components/App/App";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
