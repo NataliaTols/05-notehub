902094c08e80471cf0ebd4cbe45089d667503be9
Natalia_Tolstykh <kopylova9624@gmail.com>
2026-03-22 11:56:50 +0100

    add react-query-devtools

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,11 @@
 import ReactDOM from "react-dom/client";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import "modern-normalize";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"; import "modern-normalize";
 import App from "./components/App/App";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <QueryClientProvider client={queryClient}>
-    <App />
-    <ReactQueryDevtools initialIsOpen={false} />
-  </QueryClientProvider>,
-);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(<QueryClientProvider client={queryClient}>
+  <App />
+  <ReactQueryDevtools initialIsOpen={false} />
+</QueryClientProvider>,);
